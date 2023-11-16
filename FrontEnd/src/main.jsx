d501d5ba62a368e5480f21e720855a8988655ce6
LanJRamos <150312343+LanJRamos@users.noreply.github.com>
2023-11-16 11:44:16 +0800

    main.jsx

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
+import Register from './pages/Register'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
