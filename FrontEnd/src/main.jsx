056bdbb2413bb2749f613ce69a9d98ce388f0ee2
LanJRamos <150312343+LanJRamos@users.noreply.github.com>
2023-11-16 11:54:04 +0800

    Added the register and edited the main.jsx

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
-import Register from './pages/Register'
+import RegisterEmployee from './pages/RegisterEmp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Dashboard />
+    <RegisterEmployee/>
   </React.StrictMode>,
 )
+  
