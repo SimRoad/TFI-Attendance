52773d6b876d1fa0315b3240b318640c4333ed49
SimRoad <montanez.simonemikaela@gmail.com>
2023-11-16 09:29:21 +0800

    feat: added dashboard, small fix on login

--- FrontEnd/src/pages/Dashboard.jsx
+++ FrontEnd/src/pages/Dashboard.jsx
@@ -1,16 +1,21 @@
 import './App.css'
+import './dashboard.css'
 import Header from '../reuse/Header'
 import Footer from '../reuse/footer'
 
-function App() {
+function Dashboard() {
 
   return (
     <>
       <Header />
-        
+        <div className='body'>
+          <div className='dashDiv'>
+              <img className='dashImg' src=".\src\assets\TFIicon.png" alt="" />
+          </div>
+        </div>
       <Footer />
     </>
   )
 }
 
-export default App
+export default Dashboard
