9ee305203a46af090b95614384039b9afb8dcca2
LaurenceTest <reylaurencecapoy@gmail.com>
2023-11-17 23:05:47 +0800

    Attempted to display route
    This might not work to be honest, I just want to share to Joselito to share the pain and suffering

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,2 +1,7 @@
-import { Express } from "express";
+import express from "express"
+import {router as employeeRouter} from "./src/routes/employee.router"
 
+express.use('/test/env/employees',employeeRouter)
+express.listen(8080,()=>{
+    console.log(`Server is listening on port 8080`)
+})
--- BackEnd/src/routes/employee.router.js
+++ BackEnd/src/routes/employee.router.js
@@ -1,1 +1,9 @@
 import databaseConn from '../../database.config.js'
+import express,{Router} from 'express'
+import EmployeeController from '../controller/employee.controller.js'
+
+const router = Router()
+
+router.get(`/`,EmployeeController.findAll())
+
+export default router
