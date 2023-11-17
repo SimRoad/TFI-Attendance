c96752ced1a2ae0d94d35d61c7f130c75f567f14
Meep220 <143765941+Meep220@users.noreply.github.com>
2023-11-17 21:35:21 +0800

    Added Leave Model

---
+++ BackEnd/src/models/leaves.model.js
@@ -0,0 +1,13 @@
+import databaseConn from '../../database.config.js'
+
+class Leaves{
+    constructor(Leaves){
+        this.leaveID = Leaves.leaveID
+        this.employeeID = Leaves.employeeID
+        this.leaveName = Leaves.leaveName
+        this.daysLeft = Leave.daysLeft
+        this.leaveDesc = Leave.leaveDesc
+    }
+}
+
+export default Leaves
