e06639ed4ac6865f9c9c4f314bac9b06fad0a096
LaurenceTest <reylaurencecapoy@gmail.com>
2023-11-17 23:05:00 +0800

    Attempt to refactor according to employee model

--- BackEnd/src/controller/employee.controller.js
+++ BackEnd/src/controller/employee.controller.js
@@ -1,13 +1,11 @@
 import databaseConn from '../../database.config.js'
-import * as Employee from "employee.model.js";
+import Employee from "../models/employee.model";
 
-exports.findAll = function(req,res){
-    Employee.findAll(function(err,employee){
-        console.log('controller')
-        if(err){
-            res.send(err);
-        }
-        console.log('res',employee);
-        res.send({status: 200, data: employee})
-    })
+class EmployeeController{
+    constructor(){}
+    findAll(req,res){
+        console.log(Employee.getAll())
+    }
 }
+
+export default EmployeeController
