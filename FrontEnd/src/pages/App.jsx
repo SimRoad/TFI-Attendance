9aa0639cc6067d83bcc1fd0fc227886d4245279c
LanJRamos <150312343+LanJRamos@users.noreply.github.com>
2023-11-21 09:36:48 +0800

    Added BrowserRouting

--- FrontEnd/src/pages/App.jsx
+++ FrontEnd/src/pages/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
-import Header from '../reuse/Header'
-import Footer from '../reuse/footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Dashboard from './Dashboard'
+import Login from './Login'
+import RegisterEmployee from './RegisterEmp'
+import HolidayCalendar from './Holidays'
 
 function App() {
 
   return (
-    <>
-      <div className='body'>
-
-      </div>
-      <Header />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Dashboard />}></Route>
+        <Route path='/Login' element={<Login />}></Route>
+        <Route path='/RegisterEmployee' element={<RegisterEmployee />}></Route>
+        <Route path='/HolidayCalendar' element={<HolidayCalendar />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
