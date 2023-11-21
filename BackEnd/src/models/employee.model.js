768b1ba00245e7b3d770195bdc6e3f736204c996
LaurenceTest <reylaurencecapoy@gmail.com>
2023-11-21 10:25:41 +0800

    making sure that the merge worked

--- BackEnd/src/models/employee.model.js
+++ BackEnd/src/models/employee.model.js
@@ -24,12 +24,7 @@ export default class Employee extends GenericModel{
         this.firstName = employee.firstName
         this.middleName = employee.middleName
         this.lastName = employee.lastName
-<<<<<<< HEAD
         this.birthDate = new Date(employee.birthDate)
-=======
-        this.addressID = employee.addressID
-        this.birthdate = new Date(employee.birthDate)
->>>>>>> 18aee388bfd9dc39d071bed9a66ff7415e122f06
         this.jobPosition = employee.jobPosition
         this.currentStatus = employee.currentStatus
         this.contactNumber = employee.contactNumber
