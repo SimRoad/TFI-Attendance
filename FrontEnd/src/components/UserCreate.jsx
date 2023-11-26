e5668727c5d6c3a285f1d1c916b0b88ca30d5efb
LanJRamos <lanceramos75@gmail.com>
2023-11-26 19:22:24 +0800

    Just saving

--- FrontEnd/src/components/UserCreate.jsx
+++ FrontEnd/src/components/UserCreate.jsx
@@ -11,10 +11,10 @@ import { useNavigate } from "react-router-dom"
 const userCreate = () => {
     return(
         <>
-            <form className="max-w-sm mx-auto">
-                <div className="mb-5">
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                    <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" />
+            <form className="flex container w-64 p-5 flex-wrap rounded-md flex-col gap-4 border-accent/20 border-b-2 bg-secondary/30">
+                <div className="mb-2 block">
+                    <label htmlFor="email" value="Your email" />
+                    <input type="email" id="email" placeholder="name@flowbite.com" { ...register('email')} />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
--- FrontEnd/src/index.jsx
+++ FrontEnd/src/index.jsx
@@ -3,6 +3,7 @@ import Dashboard from './pages/Dashboard'
 import {BrowserRouter as Router, Routes, Route, Navigate, Outlet} from 'react-router-dom'
 import SessionProvider,{SessionContext} from './session/SessionProvider'
 import {useContext} from 'react'
+import EmployeeRegisterForm from './components/EmployeeRegister'
 
 const index = ()=>{
     return(
@@ -12,7 +13,8 @@ const index = ()=>{
                     <Route element={<PrivateRoutes/>}>
                         <Route element={<Dashboard/>} path='/dashboard'/>
                     </Route>
-                    <Route path='/login' element={<Login/>}></Route>
+                    {/* <Route path='/login' element={<Login/>}></Route> */}
+                    <Route path='/employeeregister' element={<EmployeeRegisterForm/>}></Route>
                 </Routes>
             </Router>
         </SessionProvider>
@@ -22,7 +24,8 @@ const index = ()=>{
 const PrivateRoutes = ({children, ...rest})=>{
     const {cookies} = useContext(SessionContext)
     return(
-        cookies.session ? <Outlet/> : <Navigate to='/login'/>
+        // cookies.session ? <Outlet/> : <Navigate to='/login'/>
+        cookies.session ? <Outlet/> : <Navigate to='/employeeregister'/>
     )
 }
 
