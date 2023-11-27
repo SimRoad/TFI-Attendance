2f7375cb54c0eae7733351eeb8071d0930ae5f49
Meep220 <143765941+Meep220@users.noreply.github.com>
2023-11-27 09:37:11 +0800

    Added icons and hrefs to sidebars...

--- FrontEnd/src/components/SideBar.jsx
+++ FrontEnd/src/components/SideBar.jsx
@@ -1,19 +1,22 @@
 'use strict'
 import { Sidebar } from "flowbite-react"
+import { FaUser , FaCalendarAlt } from "react-icons/fa";
+import { BsFillBriefcaseFill } from "react-icons/bs";
+import { MdDashboard } from "react-icons/md"
 
 function WebSideBar(){
     return (
         <Sidebar>
-            <Sidebar.Item>
+            <Sidebar.Item href="#" icon = {MdDashboard}>
                 Dashboard
             </Sidebar.Item>
-            <Sidebar.Item>
+            <Sidebar.Item href="#" icon = {FaUser}>
                 Employees
             </Sidebar.Item>
-            <Sidebar.Item>
+            <Sidebar.Item href="#" icon = {FaCalendarAlt}>
                 Shifts
             </Sidebar.Item>
-            <Sidebar.Item>
+            <Sidebar.Item href="#" icon = {BsFillBriefcaseFill}>
                 Leaves
             </Sidebar.Item>
         </Sidebar>
