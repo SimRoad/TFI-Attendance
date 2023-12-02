eb9f2a688aa6814bdee197e8b144f82839e297b7
Meep220 <143765941+Meep220@users.noreply.github.com>
2023-12-02 20:55:45 +0800

    Sql is now filled with employee and address dummy data
    is there any other data i need to add?

--- BackEnd/Employee_Mock_Data.sql
+++ BackEnd/Employee_Mock_Data.sql
@@ -1,53 +1,53 @@
-('Jocko', 'A', 'Yukhnov', 'Male', 'Legally Separated', 4, '1975-01-29', 'Human Resources', 'Fired', '2114345834', 'jyukhnov0@yelp.com');
-('Alisha', 'A', 'Goodredge', 'Female', 'Legally Separated', 5, '1971-06-19', 'Plant Factory Worker', 'Working Employee', '3554763618', 'agoodredge1@imageshack.us');
-('Wat', 'Q', 'Harrild', 'Male', 'Single', 6, '1977-10-29', 'Inventory Clerk', 'Working Employee', '4522753171', 'wharrild2@hubpages.com');
-('Roseanna', 'N', 'Doyley', 'Female', 'Widow', 7, '1996-04-30', 'Finance', 'Working Employee', '9104500575', 'rdoyley3@cbslocal.com');
-('Bertrand', 'X', 'Trask', 'Male', 'Single', 8, '1997-08-28', 'Plant Factory Worker', 'Working Employee', '9457945033', 'btrask4@guardian.co.uk');
-('Paule', 'S', 'Steggles', 'Female', 'Legally Separated', 9, '1987-08-19', 'Finance', 'Working Employee', '7252805601', 'psteggles5@issuu.com');
-('Sampson', 'X', 'Breckon', 'Male', 'Legally Separated', 10, '1999-08-31', 'Factory Worker', 'Fired', '7643244269', 'sbreckon6@networkadvertising.org');
-('Corilla', 'T', 'Point', 'Female', 'Widow', 11, '1992-02-14', 'Driver', 'Working Employee', '6024092682', 'cpoint7@gravatar.com');
-('Melba', 'N', 'Montes', 'Female', 'Single', 12, '1996-02-29', 'Plant Factory Worker', 'Fired', '1855290060', 'mmontes8@linkedin.com');
-('Retha', 'C', 'Hatton', 'Female', 'Legally Separated', 13, '1993-05-09', 'Plant Factory Worker', 'Working Employee', '8915517419', 'rhatton9@virginia.edu');
-('Gauthier', 'E', 'Fozard', 'Male', 'Legally Separated', 14, '1978-09-10', 'Finance', 'Fired', '4438972484', 'gfozarda@skyrock.com');
-('Tedd', 'E', 'Heinsen', 'Male', 'Married', 15, '1984-12-13', 'Plant Factory Worker', 'Working Employee', '7145952457', 'theinsenb@rambler.ru');
-('Julissa', 'R', 'Penhalewick', 'Female', 'Single', 16, '1989-12-17', 'Plant Factory Worker', 'Working Employee', '9426023026', 'jpenhalewickc@bing.com');
-('Rozanna', 'Y', 'Smidmoor', 'Female', 'Widow', 17, '1990-12-28', 'Plant Factory Worker', 'Working Employee', '9371704764', 'rsmidmoord@about.com');
-('Barthel', 'L', 'McMorran', 'Male', 'Single', 18, '1986-08-28', 'Factory Worker', 'Working Employee', '1516465520', 'bmcmorrane@t.co');
-('Jacquelyn', 'B', 'Dawson', 'Female', 'Widow', 19, '1994-10-24', 'Plant Factory Worker', 'Working Employee', '8413350635', 'jdawsonf@plala.or.jp');
-('Jacob', 'M', 'Di Biagi', 'Male', 'Single', 20, '1990-10-14', 'Factory Worker', 'Working Employee', '7419463996', 'jdibiagig@bloglines.com');
-('Dietrich', 'U', 'Coe', 'Male', 'Legally Separated', 21, '1990-11-22', 'Plant Factory Worker', 'Working Employee', '3895217039', 'dcoeh@prlog.org');
-('Marylou', 'J', 'Cock', 'Bigender', 'Single', 22, '1993-01-17', 'Plant Factory Worker', 'Working Employee', '1707491623', 'mcocki@purevolume.com');
-('Eldin', 'Y', 'Menure', 'Male', 'Single', 23, '2000-08-25', 'Factory Worker', 'Working Employee', '6031696567', 'emenurej@dmoz.org');
-('Hilliard', 'Y', 'Lough', 'Bigender', 'Single', 24, '1979-05-29', 'Plant Factory Worker', 'Fired', '7994705352', 'hloughk@themeforest.net');
-('Windy', 'O', 'Littleproud', 'Female', 'Married', 25, '1992-05-16', 'Factory Worker', 'Fired', '1666108626', 'wlittleproudl@newyorker.com');
-('Jamey', 'V', 'Mousdall', 'Male', 'Single', 26, '2000-05-11', 'Plant Factory Worker', 'Working Employee', '8718641497', 'jmousdallm@boston.com');
-('Gene', 'V', 'Barnwille', 'Male', 'Widow', 27, '1982-11-15', 'Plant Factory Worker', 'Working Employee', '4101051922', 'gbarnwillen@storify.com');
-('Gilligan', 'C', 'Trickett', 'Female', 'Married', 28, '1999-01-20', 'Factory Worker', 'Working Employee', '9303975403', 'gtricketto@tinyurl.com');
-('Margette', 'W', 'Petrowsky', 'Female', 'Married', 29, '1978-08-15', 'Inventory Clerk', 'Working Employee', '5722454936', 'mpetrowskyp@bigcartel.com');
-('Cobby', 'G', 'McGee', 'Male', 'Married', 30, '1978-05-27', 'Plant Factory Worker', 'Working Employee', '6537558085', 'cmcgeeq@wisc.edu');
-('Alica', 'K', 'Beecroft', 'Female', 'Legally Separated', 31, '1995-08-08', 'Factory Worker', 'Working Employee', '4938784349', 'abeecroftr@google.com.au');
-('Susanetta', 'D', 'Whooley', 'Female', 'Married', 32, '1976-06-03', 'Factory Worker', 'Working Employee', '6774669186', 'swhooleys@wikispaces.com');
-('Farlee', 'L', 'Greser', 'Male', 'Legally Separated', 33, '1998-07-01', 'Factory Worker', 'Fired', '4136288611', 'fgresert@vinaora.com');
-('Atlante', 'E', 'Avesque', 'Female', 'Legally Separated', 34, '1999-11-01', 'Driver', 'Working Employee', '1859335872', 'aavesqueu@stanford.edu');
-('Riobard', 'H', 'Kirsche', 'Male', 'Single', 35, '1978-12-16', 'Human Resources', 'Fired', '1112685185', 'rkirschev@google.it');
-('Wyatan', 'L', 'Freed', 'Genderfluid', 'Widow', 36, '1974-03-15', 'Driver', 'Fired', '4348446673', 'wfreedw@behance.net');
-('Shurlock', 'F', 'Wollard', 'Male', 'Legally Separated', 37, '1980-06-06', 'Factory Worker', 'Working Employee', '4229122308', 'swollardx@deliciousdays.com');
-('Kessia', 'H', 'Geerdts', 'Female', 'Married', 38, '1971-04-11', 'Factory Worker', 'Fired', '7138558275', 'kgeerdtsy@boston.com');
-('Sheila', 'K', 'Santon', 'Female', 'Single', 39, '1977-11-13', 'Plant Factory Worker', 'Fired', '8323338316', 'ssantonz@google.es');
-('Erek', 'P', 'Lauks', 'Male', 'Married', 40, '1982-04-11', 'Factory Worker', 'Working Employee', '2189868855', 'elauks10@usnews.com');
-('Bonnee', 'V', 'Cubbinelli', 'Female', 'Married', 41, '1987-04-19', 'Plant Factory Worker', 'Fired', '9965636793', 'bcubbinelli11@vk.com');
-('Cornela', 'L', 'Fillon', 'Female', 'Widow', 42, '2000-01-18', 'Plant Factory Worker', 'Working Employee', '4974230511', 'cfillon12@fema.gov');
-('Ebonee', 'K', 'Bewfield', 'Female', 'Married', 43, '1979-10-13', 'Driver', 'Working Employee', '6136451675', 'ebewfield13@cdbaby.com');
-('Marsh', 'S', 'Freebury', 'Male', 'Widow', 44, '1979-03-26', 'Factory Worker', 'Working Employee', '3554901166', 'mfreebury14@de.vu');
-('Aline', 'B', 'Pavelin', 'Female', 'Widow', 45, '1994-02-06', 'Plant Factory Worker', 'Working Employee', '8345291031', 'apavelin15@icq.com');
-('Marisa', 'I', 'Massot', 'Female', 'Married', 46, '1982-07-04', 'Driver', 'Working Employee', '2773442888', 'mmassot16@woothemes.com');
-('Herman', 'C', 'Courtonne', 'Male', 'Legally Separated', 47, '1979-05-04', 'Driver', 'Working Employee', '5199308377', 'hcourtonne17@tmall.com');
-('Agnes', 'P', 'Mimmack', 'Female', 'Married', 48, '1973-07-20', 'Plant Factory Worker', 'Working Employee', '6687623296', 'amimmack18@discuz.net');
-('Leigha', 'M', 'Knapton', 'Female', 'Single', 49, '1987-05-16', 'Factory Worker', 'Working Employee', '7525916977', 'lknapton19@bbb.org');
-('Jdavie', 'P', 'Witt', 'Male', 'Married', 50, '1996-07-13', 'Finance', 'Fired', '3239584592', 'jwitt1a@issuu.com');
-('Yard', 'T', 'Courtman', 'Male', 'Legally Separated', 51, '1979-11-19', 'Plant Factory Worker', 'Working Employee', '9992462160', 'ycourtman1b@chron.com');
-('Tommie', 'G', 'Bartrap', 'Male', 'Widow', 52, '1988-03-21', 'Plant Factory Worker', 'Working Employee', '2914312975', 'tbartrap1c@clickbank.net');
-('Elia', 'V', 'Nowak', 'Male', 'Widow', 53, '1978-02-06', 'Driver', 'Working Employee', '1436292952', 'enowak1d@feedburner.com');
+('Jocko', 'A', 'Yukhnov', 'Male', 'Legally Separated', 4, '1975-01-29', 'Human Resources', 'Fired', '2114345834', 'jyukhnov0@yelp.com'),
+('Alisha', 'A', 'Goodredge', 'Female', 'Legally Separated', 5, '1971-06-19', 'Plant Factory Worker', 'Working Employee', '3554763618', 'agoodredge1@imageshack.us'),
+('Wat', 'Q', 'Harrild', 'Male', 'Single', 6, '1977-10-29', 'Inventory Clerk', 'Working Employee', '4522753171', 'wharrild2@hubpages.com'),
+('Roseanna', 'N', 'Doyley', 'Female', 'Widow', 7, '1996-04-30', 'Finance', 'Working Employee', '9104500575', 'rdoyley3@cbslocal.com'),
+('Bertrand', 'X', 'Trask', 'Male', 'Single', 8, '1997-08-28', 'Plant Factory Worker', 'Working Employee', '9457945033', 'btrask4@guardian.co.uk'),
+('Paule', 'S', 'Steggles', 'Female', 'Legally Separated', 9, '1987-08-19', 'Finance', 'Working Employee', '7252805601', 'psteggles5@issuu.com'),
+('Sampson', 'X', 'Breckon', 'Male', 'Legally Separated', 10, '1999-08-31', 'Factory Worker', 'Fired', '7643244269', 'sbreckon6@networkadvertising.org'),
+('Corilla', 'T', 'Point', 'Female', 'Widow', 11, '1992-02-14', 'Driver', 'Working Employee', '6024092682', 'cpoint7@gravatar.com'),
+('Melba', 'N', 'Montes', 'Female', 'Single', 12, '1996-02-29', 'Plant Factory Worker', 'Fired', '1855290060', 'mmontes8@linkedin.com'),
+('Retha', 'C', 'Hatton', 'Female', 'Legally Separated', 13, '1993-05-09', 'Plant Factory Worker', 'Working Employee', '8915517419', 'rhatton9@virginia.edu'),
+('Gauthier', 'E', 'Fozard', 'Male', 'Legally Separated', 14, '1978-09-10', 'Finance', 'Fired', '4438972484', 'gfozarda@skyrock.com'),
+('Tedd', 'E', 'Heinsen', 'Male', 'Married', 15, '1984-12-13', 'Plant Factory Worker', 'Working Employee', '7145952457', 'theinsenb@rambler.ru'),
+('Julissa', 'R', 'Penhalewick', 'Female', 'Single', 16, '1989-12-17', 'Plant Factory Worker', 'Working Employee', '9426023026', 'jpenhalewickc@bing.com'),
+('Rozanna', 'Y', 'Smidmoor', 'Female', 'Widow', 17, '1990-12-28', 'Plant Factory Worker', 'Working Employee', '9371704764', 'rsmidmoord@about.com'),
+('Barthel', 'L', 'McMorran', 'Male', 'Single', 18, '1986-08-28', 'Factory Worker', 'Working Employee', '1516465520', 'bmcmorrane@t.co'),
+('Jacquelyn', 'B', 'Dawson', 'Female', 'Widow', 19, '1994-10-24', 'Plant Factory Worker', 'Working Employee', '8413350635', 'jdawsonf@plala.or.jp'),
+('Jacob', 'M', 'Di Biagi', 'Male', 'Single', 20, '1990-10-14', 'Factory Worker', 'Working Employee', '7419463996', 'jdibiagig@bloglines.com'),
+('Dietrich', 'U', 'Coe', 'Male', 'Legally Separated', 21, '1990-11-22', 'Plant Factory Worker', 'Working Employee', '3895217039', 'dcoeh@prlog.org'),
+('Marylou', 'J', 'Cock', 'Bigender', 'Single', 22, '1993-01-17', 'Plant Factory Worker', 'Working Employee', '1707491623', 'mcocki@purevolume.com'),
+('Eldin', 'Y', 'Menure', 'Male', 'Single', 23, '2000-08-25', 'Factory Worker', 'Working Employee', '6031696567', 'emenurej@dmoz.org'),
+('Hilliard', 'Y', 'Lough', 'Bigender', 'Single', 24, '1979-05-29', 'Plant Factory Worker', 'Fired', '7994705352', 'hloughk@themeforest.net'),
+('Windy', 'O', 'Littleproud', 'Female', 'Married', 25, '1992-05-16', 'Factory Worker', 'Fired', '1666108626', 'wlittleproudl@newyorker.com'),
+('Jamey', 'V', 'Mousdall', 'Male', 'Single', 26, '2000-05-11', 'Plant Factory Worker', 'Working Employee', '8718641497', 'jmousdallm@boston.com'),
+('Gene', 'V', 'Barnwille', 'Male', 'Widow', 27, '1982-11-15', 'Plant Factory Worker', 'Working Employee', '4101051922', 'gbarnwillen@storify.com'),
+('Gilligan', 'C', 'Trickett', 'Female', 'Married', 28, '1999-01-20', 'Factory Worker', 'Working Employee', '9303975403', 'gtricketto@tinyurl.com'),
+('Margette', 'W', 'Petrowsky', 'Female', 'Married', 29, '1978-08-15', 'Inventory Clerk', 'Working Employee', '5722454936', 'mpetrowskyp@bigcartel.com'),
+('Cobby', 'G', 'McGee', 'Male', 'Married', 30, '1978-05-27', 'Plant Factory Worker', 'Working Employee', '6537558085', 'cmcgeeq@wisc.edu'),
+('Alica', 'K', 'Beecroft', 'Female', 'Legally Separated', 31, '1995-08-08', 'Factory Worker', 'Working Employee', '4938784349', 'abeecroftr@google.com.au'),
+('Susanetta', 'D', 'Whooley', 'Female', 'Married', 32, '1976-06-03', 'Factory Worker', 'Working Employee', '6774669186', 'swhooleys@wikispaces.com'),
+('Farlee', 'L', 'Greser', 'Male', 'Legally Separated', 33, '1998-07-01', 'Factory Worker', 'Fired', '4136288611', 'fgresert@vinaora.com'),
+('Atlante', 'E', 'Avesque', 'Female', 'Legally Separated', 34, '1999-11-01', 'Driver', 'Working Employee', '1859335872', 'aavesqueu@stanford.edu'),
+('Riobard', 'H', 'Kirsche', 'Male', 'Single', 35, '1978-12-16', 'Human Resources', 'Fired', '1112685185', 'rkirschev@google.it'),
+('Wyatan', 'L', 'Freed', 'Genderfluid', 'Widow', 36, '1974-03-15', 'Driver', 'Fired', '4348446673', 'wfreedw@behance.net'),
+('Shurlock', 'F', 'Wollard', 'Male', 'Legally Separated', 37, '1980-06-06', 'Factory Worker', 'Working Employee', '4229122308', 'swollardx@deliciousdays.com'),
+('Kessia', 'H', 'Geerdts', 'Female', 'Married', 38, '1971-04-11', 'Factory Worker', 'Fired', '7138558275', 'kgeerdtsy@boston.com'),
+('Sheila', 'K', 'Santon', 'Female', 'Single', 39, '1977-11-13', 'Plant Factory Worker', 'Fired', '8323338316', 'ssantonz@google.es'),
+('Erek', 'P', 'Lauks', 'Male', 'Married', 40, '1982-04-11', 'Factory Worker', 'Working Employee', '2189868855', 'elauks10@usnews.com'),
+('Bonnee', 'V', 'Cubbinelli', 'Female', 'Married', 41, '1987-04-19', 'Plant Factory Worker', 'Fired', '9965636793', 'bcubbinelli11@vk.com'),
+('Cornela', 'L', 'Fillon', 'Female', 'Widow', 42, '2000-01-18', 'Plant Factory Worker', 'Working Employee', '4974230511', 'cfillon12@fema.gov'),
+('Ebonee', 'K', 'Bewfield', 'Female', 'Married', 43, '1979-10-13', 'Driver', 'Working Employee', '6136451675', 'ebewfield13@cdbaby.com'),
+('Marsh', 'S', 'Freebury', 'Male', 'Widow', 44, '1979-03-26', 'Factory Worker', 'Working Employee', '3554901166', 'mfreebury14@de.vu'),
+('Aline', 'B', 'Pavelin', 'Female', 'Widow', 45, '1994-02-06', 'Plant Factory Worker', 'Working Employee', '8345291031', 'apavelin15@icq.com'),
+('Marisa', 'I', 'Massot', 'Female', 'Married', 46, '1982-07-04', 'Driver', 'Working Employee', '2773442888', 'mmassot16@woothemes.com'),
+('Herman', 'C', 'Courtonne', 'Male', 'Legally Separated', 47, '1979-05-04', 'Driver', 'Working Employee', '5199308377', 'hcourtonne17@tmall.com'),
+('Agnes', 'P', 'Mimmack', 'Female', 'Married', 48, '1973-07-20', 'Plant Factory Worker', 'Working Employee', '6687623296', 'amimmack18@discuz.net'),
+('Leigha', 'M', 'Knapton', 'Female', 'Single', 49, '1987-05-16', 'Factory Worker', 'Working Employee', '7525916977', 'lknapton19@bbb.org'),
+('Jdavie', 'P', 'Witt', 'Male', 'Married', 50, '1996-07-13', 'Finance', 'Fired', '3239584592', 'jwitt1a@issuu.com'),
+('Yard', 'T', 'Courtman', 'Male', 'Legally Separated', 51, '1979-11-19', 'Plant Factory Worker', 'Working Employee', '9992462160', 'ycourtman1b@chron.com'),
+('Tommie', 'G', 'Bartrap', 'Male', 'Widow', 52, '1988-03-21', 'Plant Factory Worker', 'Working Employee', '2914312975', 'tbartrap1c@clickbank.net'),
+('Elia', 'V', 'Nowak', 'Male', 'Widow', 53, '1978-02-06', 'Driver', 'Working Employee', '1436292952', 'enowak1d@feedburner.com'),
 
 
  4_Employee.png
