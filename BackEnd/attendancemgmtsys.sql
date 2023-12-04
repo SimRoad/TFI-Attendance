34feeeb4890bf646d1d8019e2a9a2bdb92159dab
Meep220 <143765941+Meep220@users.noreply.github.com>
2023-12-04 23:49:39 +0800

    Removed HolidayType from sql

--- BackEnd/attendancemgmtsys.sql
+++ BackEnd/attendancemgmtsys.sql
@@ -456,15 +456,15 @@ INSERT INTO `employee` (firstName,middleName,lastName,gender,civilStatus,address
 ('Yard', 'T', 'Courtman', 'Male', 'Legally Separated', '51', '1979-11-19', 'Plant Factory Worker', 'Working Employee', '9992462160', 'ycourtman1b@chron.com','51_Employee.png'),
 ('Tommie', 'G', 'Bartrap', 'Male', 'Widow', '52', '1988-03-21', 'Plant Factory Worker', 'Working Employee', '2914312975', 'tbartrap1c@clickbank.net','52_Employee.png'),
 ('Elia', 'V', 'Nowak', 'Male', 'Widow', '53', '1978-02-06', 'Driver', 'Working Employee', '1436292952', 'enowak1d@feedburner.com','53_Employee.png');
-INSERT INTO `holidays` (holidayName,holidayDate,holidayType) VALUES
-('Bonifacio Day','2023-11-27','Regular'),
-("All Saints' Day",'2023-11-1','Regular'),
-("All Souls' Day", '2023-11-2','Regular'),
-("New Years Day", '2024-1-1','Regular'),
-("Labor Day", '2024-5-1', 'Regular'),
-("Independence Day", '2024-6-12', 'Regular'),
-("Rizal Day",'2023-12-30','Regular'),
-("Christmas Day",'2023-12-25','Regular') 
+INSERT INTO `holidays` (holidayName,holidayDate) VALUES
+('Bonifacio Day','2023-11-27'),
+("All Saints' Day",'2023-11-1'),
+("All Souls' Day", '2023-11-2'),
+("New Years Day", '2024-1-1'),
+("Labor Day", '2024-5-1'),
+("Independence Day", '2024-6-12'),
+("Rizal Day",'2023-12-30'),
+("Christmas Day",'2023-12-25') 
 ;
 INSERT INTO `leaves` (`leavesID`, `leaveName`) VALUES
 (1, 'Sick Leave',5),
