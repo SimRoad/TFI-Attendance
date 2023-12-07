741b5324217fb3bc09f010f53fee84b682c45d8e
LanJRamos <150312343+LanJRamos@users.noreply.github.com>
2023-12-07 11:31:42 +0800

    Forgot about this lmao

---
+++ FrontEnd/src/components/Debug.jsx
@@ -0,0 +1,13 @@
+import EmployeeTable from "./EmployeeTable"
+
+const debug = () => {
+    return(
+        <>
+            <div className="grid grid-flow-col auto-cols-max">
+                <EmployeeTable/>
+            </div>
+        </>
+    )
+}
+
+export default debug
